ea3f310644d0737e0a186bd0c9be64007497f2ae
Durgesh <scs.durgesh@gmail.com>
2016-04-27 22:45:20 +0530

    Design incomplete

--- src/main/MainMethod.java
+++ src/main/MainMethod.java
@@ -17,74 +17,61 @@ public class MainMethod {
 		Trello trello = new TrelloImpl("3d8097336659354b2b057ae281064004",
 				"9f76b28622d45f8b00f6e0b6a59e8a999e0b62172363725ea13347bb8d42adc5");
 
-		String BoardId="571f2e85e198e2bba1302828";////Board Durgesh
-//		String BoardId="566aa55d83a50f627edda222";// Board Kanban
-//		String BoardId="566aa55d83a50f627edda222";// Board Example
-		
-		ArrayList<Details> detailList=new ArrayList<Details>();
-		
-		
-		
+		String BoardId = "571f2e85e198e2bba1302828";// //Board Durgesh
+		// String BoardId="566aa55d83a50f627edda222";// Board Kanban
+		// String BoardId="566aa55d83a50f627edda222";// Board Example
+
+		ArrayList<Details> detailList = new ArrayList<Details>();
+
 		List<org.trello4j.model.List> lists = trello.getListByBoard(BoardId);
-		String boardName=trello.getBoard(BoardId).getName();
-		System.out.println("Board Name :"+boardName);
-		
+		String boardName = trello.getBoard(BoardId).getName();
+
 		Iterator<org.trello4j.model.List> itrl = lists.iterator();
 		while (itrl.hasNext()) {
-			Details det=new Details();
+			Details det = new Details();
 			det.setBoardName(boardName);
 			org.trello4j.model.List ll = itrl.next();
-//			System.out.println("List :"+ll.getName());
 			det.setListName(ll.getName());
-			
+
 			java.util.List<Card> list = trello.getCardsByList(ll.getId());
 			Iterator<Card> itr = list.iterator();
-			
-//			System.out.println("Total Cards :"+list.size());
-			
+			if (list.size() == 0) {
+				det.setCardName("No Cards");
+				det.setCardStatus("No Travels");
+			}
+
 			while (itr.hasNext()) {
 				Card obj = itr.next();
-				System.out.println("card :"+obj.getName());
-//				det.setCardName(obj.getName());
-				List<Action> act=trello.getActionsByCard(obj.getId());
-				if(act.size()>1)
-				{
-					Date stopDate=act.get(0).getDate();
-					int x=act.size()-1;
-					Date startDate=act.get(x).getDate();
-					DateBeans diffDate=differenceDate(startDate,stopDate);
-					/*System.out.println("Size :"+act.size());
-					System.out.println("Start date :"+startDate );
-					System.out.println("Stop date :"+stopDate);
-					System.out.println("Diff :"+diffDate);
-					*/det.setTravelDetails(diffDate);
-//					System.out.println();
-				}else
-				{
-					det.setCardName("No Travels");
-//					System.out.println("Card Status : No Travels");
+				det.setCardName(obj.getName());
+				List<Action> act = trello.getActionsByCard(obj.getId());
+				if (act.size() > 1) {
+					Date stopDate = act.get(0).getDate();
+					int x = act.size() - 1;
+					Date startDate = act.get(x).getDate();
+					TimeDifference diff = new TimeDifference();
+					DateBeans diffDate = diff.differenceDate(startDate,
+							stopDate);
+					det.setTravelDetails(diffDate);
+				} else {
+					det.setCardStatus("No Travels");
 				}
 			}
 			detailList.add(det);
 		}
-
+		printList(detailList);
 	}
 
-	
-	
-	private static DateBeans differenceDate(Date startDate, Date stopDate) {
-		// TODO Auto-generated method stub
-		long diff = stopDate.getTime() - startDate.getTime();
-		long diffSeconds = diff / 1000 % 60;
-		long diffMinutes = diff / (60 * 1000) % 60;
-		long diffHours = diff / (60 * 60 * 1000) % 24;
-		long diffDays = diff / (24 * 60 * 60 * 1000);
-		DateBeans bean=new DateBeans();
-		bean.setDiffDays(diffDays);
-		bean.setDiffHour(diffHours);
-		bean.setDiffMin(diffMinutes);
-		bean.setDiffSec(diffSeconds);
-		return bean;
+	private static void printList(ArrayList<Details> detailList) {
+
+		for (int i = 0; i < detailList.size(); i++) {
+			Details det = detailList.get(i);
+			System.out.println("Board Name :" + det.getBoardName());
+			System.out.println("Card Name :" + det.getCardName());
+			System.out.println("Card Status :" + det.getCardStatus());
+			System.out.println("List Name :" + det.getListName());
+			System.out.println("Card Travels :" + det.getTravelDetails());
+			System.out.println();
+		}
 
 	}
 
--- src/main/TimeDifference.java
+++ src/main/TimeDifference.java
@@ -1,59 +1,23 @@
 package main;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeDifference {
 
-	public DateBeans getDifference(String dateStart, String dateStop) {
 
-		// HH converts hour in 24 hours format (0-23), day calculation
-		String startDateformat[] = dateStart.split("T");
-		String updatedStartDate = startDateformat[0] + " " + startDateformat[1];
-		System.out.println(updatedStartDate);
+	public  DateBeans differenceDate(Date startDate, Date stopDate) {
+		// TODO Auto-generated method stub
+		long diff = stopDate.getTime() - startDate.getTime();
+		long diffSeconds = diff / 1000 % 60;
+		long diffMinutes = diff / (60 * 1000) % 60;
+		long diffHours = diff / (60 * 60 * 1000) % 24;
+		long diffDays = diff / (24 * 60 * 60 * 1000);
+		DateBeans bean=new DateBeans();
+		bean.setDiffDays(diffDays);
+		bean.setDiffHour(diffHours);
+		bean.setDiffMin(diffMinutes);
+		bean.setDiffSec(diffSeconds);
+		return bean;
 
-		String stopDateformat[] = dateStop.split("T");
-		String updatedStopDate = stopDateformat[0] + " " + stopDateformat[1];
-		System.out.println(updatedStopDate);
-
-		// HH converts hour in 24 hours format (0-23), day calculation
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-		Date d1 = null;
-		Date d2 = null;
-
-		try
-
-		{
-			d1 = format.parse(updatedStartDate);
-			d1 = format.parse(updatedStartDate);
-			d2 = format.parse(updatedStopDate);
-
-			// in milliseconds
-			long diff = d2.getTime() - d1.getTime();
-
-			long diffSeconds = diff / 1000 % 60;
-			long diffMinutes = diff / (60 * 1000) % 60;
-			long diffHours = diff / (60 * 60 * 1000) % 24;
-			long diffDays = diff / (24 * 60 * 60 * 1000);
-
-			DateBeans bean=new DateBeans();
-			bean.setDiffDays(diffDays);
-			bean.setDiffHour(diffHours);
-			bean.setDiffMin(diffMinutes);
-			bean.setDiffSec(diffSeconds);
-			return bean;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static void main(String[] args) {
-
-		TimeDifference dif = new TimeDifference();
-		String dateStart = "2012-01-14T09:29:58";
-		String dateStop = "2012-01-15T10:31:48";
-		dif.getDifference(dateStart, dateStop);
 	}
 }
